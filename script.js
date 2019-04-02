7bc5ed7e941646b0afac6dd6c68fa43e364943ee
Pier.W <factory666@live.it>
2019-04-02 16:05:02 +0200

    function deleteCard ok

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 $(document).ready(init);
 
+
+function clearCard() {
+
+  $(".card").remove();
+}
+
 function getCard() {
+
+  clearCard();
 
   $.ajax({
     url: "http://157.230.17.132:3021/todos",
@@ -35,10 +43,29 @@ function getCard() {
   });
 }
 
+function deleteCard() {
 
+  var me = $(this);
+  var id = me.siblings(".card-id").text();
 
+  $.ajax({
+    url: "http://157.230.17.132:3021/todos/" + id,
+    method: "DELETE",
+    success: function(apiData, stato) {
+
+      if (stato == "success") {
+
+        getCard();
+      }
+    },
+    error: function(richesta, stato, errori) {
+      alert("Problemi di connessione");
+    },
+  });
+}
 
 function init() {
 
   getCard();
+  $(document).on("click", ".card > ul > span", deleteCard)
 }
